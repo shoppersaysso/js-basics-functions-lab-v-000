4ab61ac86b823e97f82b038003a1b75ec1c050aa
Sara Hopper <sara.hopper@gmail.com>
2017-09-27 11:08:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+function calculateVertical(start, destination) {
+  return ((destination - start)* 264)
+}
+
+function distanceFromHqInBlocks(blockNumber) {
+  if (blockNumber > 42) {
+    return blockNumber - 42;
+  } else {
+    return 42 - blockNumber;
+  }
+}
+
+
+function distanceFromHqInFeet(blockNumber) {
+  return distanceFromHqInBlocks(blockNumber) * 264;
+
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(calculateVertical(destination, start))
+}
+
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance * .02);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+
+}
