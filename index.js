f23f43dcaaa309beae7cc3d973733f9e0b399fb6
Sara Hopper <sara.hopper@gmail.com>
2017-09-27 11:01:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,16 +21,16 @@ function distanceTravelledInFeet(start, destination) {
 }
 
 
-function calculatesFarePrice(start, destination) {
-  const distance = distanceTravelledInFeet(start, destination)
-  if distance >= 400 && distance <= 2000 {
-    return distance * .02
-  } else {
-    if distance > 2000 {
-      return 25
-    } else {
-      return 'cannot travel that far'
-    }
-  }
-  
-}
+// function calculatesFarePrice(start, destination) {
+//   const distance = distanceTravelledInFeet(start, destination)
+//   if distance >= 400 && distance <= 2000 {
+//     return distance * .02
+//   } else {
+//     if distance > 2000 {
+//       return 25
+//     } else {
+//       return 'cannot travel that far'
+//     }
+//   }
+// 
+// }
