fe3fc8824fa952e34901719fb12c9261d8ce0877
Sara Hopper <sara.hopper@gmail.com>
2017-09-27 11:04:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ function distanceTravelledInFeet(start, destination) {
 
 function calculatesFarePrice(start, destination) {
   const distance = distanceTravelledInFeet(start, destination)
+  
   if distance >= 400 && distance <= 2000
     return distance * .02;
   } else {
